5068b0b1fe23c61100c2e71925b769b475dc19cd
Ome Asraf <omeasraf0@gmail.com>
2021-04-22 15:28:57 -0400

    Update SFIconsTests.swift

--- Tests/SFIconsTests/SFIconsTests.swift
+++ Tests/SFIconsTests/SFIconsTests.swift
@@ -6,7 +6,7 @@ final class TestFileTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-//        XCTAssertEqual(SFIcons().text, "Hello, World!")
+        XCTAssertEqual(SFIcons.circle, "circle")
     }
 
     static var allTests = [
